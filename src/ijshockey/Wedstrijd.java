107050a84a23c28e015d0a02bb6972f7c558d3e5
jdverman <jasper.vermandere@gmail.com>
2015-12-01 12:10:23 +0100

    spelerrapport

--- src/ijshockey/Wedstrijd.java
+++ src/ijshockey/Wedstrijd.java
@@ -109,4 +109,9 @@ public class Wedstrijd {
         this.gespeeld = gespeeld;
     }
 
+    @Override
+    public String toString() {
+        return thuisTeam + " - " + uitTeam + " " + datum;
+    }
+
 }
